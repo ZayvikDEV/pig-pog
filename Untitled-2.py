448579e035b3a45ba84a891c2540b5c905e00063
lox3000 <81582636+lox3000@users.noreply.github.com>
2021-04-12 19:54:34 +0300

    1.1
    Фон

--- Untitled-2.py
+++ Untitled-2.py
@@ -2,12 +2,12 @@ from pygame import *
 from random import randint
 
 mixer.init()
-mixer.music.load('')
+mixer.music.load("M.mp3")
 
-img_back = "galaxy.jpg"
+img_back = "F.jpg"
 
 font.init()
-font = font.SysFont('Arial', 80)
+font = font.SysFont('Arial', 40)
 
 score = -99
 lost = 0 
@@ -37,8 +37,8 @@ class Player(GameSprite):
         bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -13)
         bullets.add(bullet)
 
-win_width = 2000
-win_height = 2000
+win_width = 1200
+win_height = 800
 display.set_caption("Shish")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
@@ -57,28 +57,18 @@ while run:
     if not finish:
         window.blit(background,(0,0))
         
-        text_goal = font2.render("Goal: " + str(goal), 1, (255, 255, 255))
-        window.blit(text_goal,(10, 10))
+        text_goal = font.render("Goal: " + str(goal), 1, (255, 255, 255))
+        window.blit(text_goal,(15, 15))
 
-        text = font2.render("Score: " + str(score), 1, (255, 255, 255))
-        window.blit(text,(10,40))
+        text = font.render("Score: " + str(score), 1, (255, 255, 255))
+        window.blit(text,(15,45))
 
-        text_lose = font2.render("Lost: " + str(lost) + " of 3", 1, (255, 255, 255))
-        window.blit(text_lose,(10,70))
+        text_lose = font.render("Lost: " + str(lost) + " of 3", 1, (255, 255, 255))
+        window.blit(text_lose,(15,75))
 
-        ship.update()
-        monsters.update()
-        bullets.update()
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
-
-        collides = sprite.groupcollide(monsters, bullets, True, True)
-        for c in collides:
-            score += 1
-            monster = Enemy(img_enemy, randint(80, win_width - 180), -40, 80, 50, randint(1, 5))
-            monsters.add(monster)
         
+        
+
         if life == 0 or lost >= max_lost:
             finish = True
             window.blit(lose, (200, 200))
