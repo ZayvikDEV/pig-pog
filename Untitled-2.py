99f974b7535101245e7008d62d0989df05d2da0e
lox3000 <81582636+lox3000@users.noreply.github.com>
2021-04-19 18:55:25 +0300

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,92 +1,88 @@
 from pygame import *
-from random import randint
-
-mixer.init()
-mixer.music.load("M.mp3")
-
-img_back = "F.jpg"
-img_rocket = "R.jpg"
-img_myach = "Y.png"
-
-font.init()
-font = font.SysFont('Arial', 40)
-
-score = -99
-lost = 0 
-max_lost = 3
-goal = 10
-life = 3
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
+
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -13)
-        bullets.add(bullet)
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 160:
+            self.rect.y += self.speed
 
-win_width = 1200
-win_height = 800
-display.set_caption("Shish")
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 160:
+            self.rect.y += self.speed
+
+back = (0, 71, 142)
+win_width = 600
+win_height = 500
 window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
-
-Ravo = Player(img_rocket, win_width - 40, 50, 30, 30, 20)
-Lavo = Player(img_rocket, win_width - 960, 50, 30, 30, 20)
-
+display.set_caption('pig-pog')
+window.fill(back)
+game = True
 finish = False
-run = True 
-while run:
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('FFF.jpg', 30, 200, 4, 50, 150)
+racket2 = Player('FFF.jpg', 520, 200, 4, 50, 150)
+ball = GameSprite('Y.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
     for e in event.get():
         if e.type == QUIT:
-            run = False
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                fire_sound.play()
-                ship.fire()
+            game = False
 
-    if not finish:
-        window.blit(background,(0,0))
-        
-        text_goal = font.render("Goal: " + str(goal), 1, (255, 255, 255))
-        window.blit(text_goal,(15, 15))
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-        text = font.render("Score: " + str(score), 1, (255, 255, 255))
-        window.blit(text,(15,45))
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
 
-        text_lose = font.render("Lost: " + str(lost) + " of 3", 1, (255, 255, 255))
-        window.blit(text_lose,(15,75))
-
-        
-        
-
-        if life == 0 or lost >= max_lost:
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
             finish = True
-            window.blit(lose, (200, 200))
-        
-        if score >= goal:
-            finish = True
-            window.blit(win, (200, 200))
+            window.blit(lose1, (200, 200))
+            game_over = True
 
-        display.update()
-    time.delay(50)
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
